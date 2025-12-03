a1151139280dd937d06bc445233b2a00c69e7fe5
djrobson <djrobson@users.noreply.github.com>
2025-12-03 11:36:31 -0500

    day03 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -42,8 +42,43 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(total)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+fn get_largest_with_room(bank: &Vec<u32>, starting: usize, ending: usize) -> usize {
+    let mut index = 0;
+    let mut val = 0;
+    for idx in starting..=bank.len()-ending {
+        if bank[idx] > val {
+            index = idx;
+            val = bank[idx];
+        }
+    }
+    index
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let banks = parse_input(input);
+    let mut total: usize = 0;
+
+    for bank in banks {
+
+        let mut nth = 12;
+        let mut result: Vec<usize> = Vec::new();
+        let mut prev_index = 0;
+        while nth > 0 {
+            let best_option = get_largest_with_room(&bank, prev_index, nth);
+            result.push(best_option);
+            prev_index = best_option+1;
+            nth -= 1;
+        }
+        let mut num = 0;
+        for res in result {
+            num += bank[res] as usize;
+            num *= 10
+        }
+        num /= 10;
+        total += num;
+
+    }
+    Some(total)
 }
 
 #[cfg(test)]
@@ -62,6 +97,16 @@ mod tests {
         assert_eq!(result, Some(expected));
     }
 
+    #[rstest]
+    #[case("987654321111111", 987654321111)]
+    #[case("811111111111119", 811111111119)]
+    #[case("234234234234278", 434234234278)]
+    #[case("818181911112111", 888911112111)]
+    fn test_part_two_example(#[case] input: &str, #[case] expected: usize) {
+        let result = part_two(input);
+        assert_eq!(result, Some(expected));
+    }
+
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
@@ -71,6 +116,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
