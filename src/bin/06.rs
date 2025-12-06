60f6406b22266b7280cf30b07b2bf20a1d92ad60
djrobson <djrobson@users.noreply.github.com>
2025-12-06 12:13:25 -0500

    day06 rewrote for arbitraty row count

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -9,8 +9,38 @@ struct Homework {
     operators: Vec<Operator>,
 }
 
-fn parse_input(input: &str) -> Homework {
+fn parse_input1(input: &str) -> Homework {
     let mut lines = input.lines();
+    let mut nums = Vec::new();
+    let mut operators = Vec::new();
+
+    lines.for_each(|line| {
+        if line.as_bytes()[0] == b'*' || line.as_bytes()[0] == b'+' {
+
+            line.split_whitespace().for_each(
+                |oper: &str| {
+                    match oper {
+                        "+" => operators.push(Operator::Add),
+                        "*" => operators.push(Operator::Multiply),
+                        _ => {},
+                    }
+                });
+        } else {
+            nums.push(line.split_whitespace().flat_map(|num: &str| num.parse()).collect());
+        }
+    });
+
+    Homework {
+        nums: nums,
+        operators: operators,
+    }
+}
+
+
+fn parse_input2(input: &str) -> Homework {
+    let mut lines = input.lines();
+
+    // iterate through the operator row, split all collumns just left of each operator, then do columnwise collection on the numbers
     let nums1: Vec<u64> = lines.next().unwrap().split_whitespace().flat_map(|num: &str| num.parse()).collect();
     let nums2: Vec<u64> = lines.next().unwrap().split_whitespace().flat_map(|num: &str| num.parse()).collect();
     let nums3: Vec<u64> = lines.next().unwrap().split_whitespace().flat_map(|num: &str| num.parse()).collect();
@@ -31,7 +61,35 @@ fn parse_input(input: &str) -> Homework {
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let homework = parse_input(input);
+    let homework = parse_input1(input);
+    let mut total = 0;
+    let rows = homework.nums.len();
+    let cols = homework.nums[0].len();
+    for col in 0..cols {
+        total += match homework.operators[col] {
+            Operator::Add => {
+                let mut col_total = 0;
+                for row in 0..rows {
+                    col_total += homework.nums[row][col];
+                }
+                //println!("col {} added to {}", col, col_total);
+                col_total
+            }
+            Operator::Multiply => {
+                let mut col_total = 1;
+                for row in 0..rows {
+                    col_total *= homework.nums[row][col];
+                }
+                //println!("col {} multiplied to {}", col, col_total);
+                col_total
+            }
+        }
+    }
+    Some(total)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let homework = parse_input2(input);
     let mut total = 0;
     for col in 0..homework.nums[0].len() {
         total += match homework.operators[col] {
@@ -40,10 +98,6 @@ pub fn part_one(input: &str) -> Option<u64> {
         }
     }
     Some(total)
-}
-
-pub fn part_two(input: &str) -> Option<u64> {
-    None
 }
 
 #[cfg(test)]
@@ -59,6 +113,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3263827));
     }
 }
