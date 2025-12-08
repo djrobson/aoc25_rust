59ec068e6ec7ef323f1f190f423dcdfd1f504c1f
djrobson <djrobson@users.noreply.github.com>
2025-12-07 19:07:44 -0500

    day07 part2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,11 +1,22 @@
 advent_of_code::solution!(7);
+use std::fmt;
 
-#[derive(Clone,Copy,PartialEq,Eq, Debug)]
+#[derive(Clone,Copy,PartialEq,Eq)]
 enum Spot {
     Empty,
     //Start,
     Split,
-    Beam,
+    Beam(usize),
+}
+impl fmt::Debug for Spot {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Spot::Empty => write!(f,"."),
+            //Start,
+            Spot::Split => write!(f,"^"),
+            Spot::Beam(count) => write!(f,"{}", count%10 ),
+        }
+    }
 }
 
 fn parse_input(input: &str) -> Vec<Vec<Spot>> {
@@ -14,11 +25,10 @@ fn parse_input(input: &str) -> Vec<Vec<Spot>> {
             let spot_val = 
             match c {
                 '.' => Spot::Empty,
-                'S' => Spot::Beam,
+                'S' => Spot::Beam(1),
                 '^' => Spot::Split,
                 _ => panic!("unexpected input {}", c)
             };
-            //println!("{}->{:?}", c, spot_val);
             spot_val
         }).collect()
     }).collect()
@@ -30,18 +40,18 @@ pub fn part_one(input: &str) -> Option<u64> {
     let line_len = grid[0].len();
     let mut line_idx = 1;
     let mut this_line: Vec<Spot> = grid[0].clone();
-    let mut next_line: Vec<Spot> = vec![Spot::Empty; line_len];
+    let mut next_line: Vec<Spot>;
     while line_idx < grid.len() {
         next_line = vec![Spot::Empty; line_len];
         for spot in 0..line_len {
-            if this_line[spot] == Spot::Beam {
+            if let Spot::Beam(_count) = this_line[spot] {
                 // check if any beams run into any splitters
                 if grid[line_idx][spot] == Spot::Split {
                     total_splits += 1;
-                    next_line[spot-1] = Spot::Beam;
-                    next_line[spot+1] = Spot::Beam;
+                    next_line[spot-1] = Spot::Beam(1);
+                    next_line[spot+1] = Spot::Beam(1);
                 }   else {
-                    next_line[spot] = Spot::Beam;
+                    next_line[spot] = Spot::Beam(1);
                 }
             } 
         }
@@ -51,8 +61,43 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(total_splits)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let grid = parse_input(input);
+    let line_len = grid[0].len();
+    let mut line_idx = 1;
+    let mut this_line: Vec<Spot> = grid[0].clone();
+    let mut next_line: Vec<Spot>;
+    while line_idx < grid.len() {
+        next_line = vec![Spot::Empty; line_len];
+        //println!("this line {:?}", this_line);
+        for spot in 0..line_len {
+            if let Spot::Beam(count) = this_line[spot] {
+                // check if any beams run into any splitters
+                if grid[line_idx][spot] == Spot::Split {
+                    if let Spot::Beam(bottom_left_count) = next_line[spot-1] {
+                        next_line[spot-1] = Spot::Beam(bottom_left_count + count);
+                    } else {
+                        next_line[spot-1] = Spot::Beam(count);
+                    }
+                    next_line[spot+1] = Spot::Beam(count);
+                } else if let Spot::Beam(straight_down_count) = next_line[spot] {
+                    // check if a split just to our left needs to merge with one going straight down
+                    next_line[spot] = Spot::Beam(count + straight_down_count);
+                } else {
+                    next_line[spot] = Spot::Beam(count);
+                }
+            } 
+        }
+        line_idx += 1;
+        std::mem::swap(&mut this_line, &mut next_line);
+    }
+    Some(this_line.iter().map(|spot: &Spot| {
+        match spot {
+            Spot::Empty => 0,
+            Spot::Split => panic!("there was a split in the last line"),
+            Spot::Beam(count) => *count,
+        }
+    }).sum())      
 }
 
 #[cfg(test)]
@@ -68,6 +113,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(40));
     }
 }
