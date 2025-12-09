dff4efb90039cc151e1ffe6723bf51511f9f4a6a
djrobson <djrobson@users.noreply.github.com>
2025-12-09 13:05:14 -0500

    day09 part2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,3 +1,6 @@
+use std::u64;
+use itertools::{self, Itertools};
+
 advent_of_code::solution!(9);
 
 fn parse_input(input: &str) -> Vec<(i64,i64)> {
@@ -24,8 +27,25 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(biggest_area)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let mut biggest_area = 0;
+    let corners = parse_input(input);
+    let lines = corners.iter().circular_tuple_windows().take(corners.len()).collect::<Vec<(&(i64,i64),&(i64,i64))>>();
+    let mut max_boxes = corners.iter().tuple_combinations().map(|(a,b)| {
+        let area = ((a.0 -b.0).abs()+1) * ((a.1 - b.1).abs()+1);
+        (a,b,area)
+    }).sorted_by_key(|b| b.2).rev();
+
+    let max_box = max_boxes.find(|(a,b,area)| {
+        lines.iter().all(|(start,end)| {
+            let left_of_rec = a.0.max(b.0) <= start.0.min(end.0);
+            let right_of_rec = a.0.min(b.0) >= start.0.max(end.0);
+            let above = a.1.max(b.1) <= start.1.min(end.1);
+            let below = a.1.min(b.1) >= start.1.max(end.1);
+            left_of_rec || right_of_rec || above || below
+        })
+    }).unwrap();
+    Some(max_box.2)
 }
 
 #[cfg(test)]
@@ -41,6 +61,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(24));
     }
 }
